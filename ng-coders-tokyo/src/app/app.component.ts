9d27daede4db582d347c06dcb7368be60770f50b
canhlx29@gmail.com <canhlx29@gmail.com>
2020-03-24 17:49:50 +0700

    canhlx

--- ng-coders-tokyo/src/app/app.component.ts
+++ ng-coders-tokyo/src/app/app.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  OnChanges,
+  OnDestroy,
+  AfterViewInit,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterContentChecked
+} from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  title = 'ng-coders-tokyo';
+export class AppComponent
+  implements
+    OnInit,
+    OnChanges,
+    OnDestroy,
+    AfterViewInit,
+    AfterContentInit,
+    AfterViewChecked,
+    AfterContentChecked {
+  ngAfterContentChecked(): void {
+    console.log("Parent AfterContentChecked ran");
+  }
+  ngAfterViewChecked(): void {
+    console.log("Parent AfterViewChecked ran");
+  }
+  ngAfterContentInit(): void {
+    console.log("Parent AfterContentInit ran");
+  }
+  ngAfterViewInit(): void {
+    console.log("Parent AfterViewInit ran");
+  }
+  ngOnDestroy(): void {
+    console.log("Parent OnDestroy ran");
+  }
+  ngOnChanges(changes: import("@angular/core").SimpleChanges): void {
+    console.log("Parent OnChanges ran", { changes });
+  }
+  ngOnInit(): void {
+    console.log("Parent OnInit ran");
+  }
+  title = "ng-coders-tokyo";
+  sendTextToChild() {
+    this.title = "Charge From Parent!";
+  }
+  changedFromHiComponent(event) {
+    this.title = event;
+  }
 }
--- ng-coders-tokyo/src/app/components/hi/hi.component.ts
+++ ng-coders-tokyo/src/app/components/hi/hi.component.ts
@@ -1,15 +1,59 @@
-import { Component, OnInit } from "@angular/core";
+import {
+  Component,
+  OnInit,
+  OnChanges,
+  OnDestroy,
+  AfterViewInit,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterContentChecked,
+  Input,
+  Output,
+  EventEmitter
+} from "@angular/core";
 
 @Component({
   selector: "app-hi",
   templateUrl: "./hi.component.html",
   styleUrls: ["./hi.component.css"]
 })
-export class HiComponent implements OnInit {
+export class HiComponent
+  implements
+    OnInit,
+    OnChanges,
+    OnDestroy,
+    AfterViewInit,
+    AfterContentInit,
+    AfterViewChecked,
+    AfterContentChecked {
+  @Input() text: string;
+  @Output() clickBtn: EventEmitter<any> = new EventEmitter<any>();
+  ngAfterContentChecked(): void {
+    console.log("AfterContentChecked ran");
+  }
+  ngAfterViewChecked(): void {
+    console.log("Child AfterViewChecked ran");
+  }
+  ngAfterContentInit(): void {
+    console.log("Child AfterContentInit ran");
+  }
+  ngAfterViewInit(): void {
+    console.log("Child AfterViewInit ran");
+  }
+  ngOnDestroy(): void {
+    console.log("Child OnDestroy ran");
+  }
+  ngOnChanges(changes: import("@angular/core").SimpleChanges): void {
+    console.log("Child OnChanges ran", { changes });
+  }
+  ngOnInit(): void {
+    console.log("Child OnInit ran");
+  }
   textColor = "tomato";
   withBorder = true;
   onButtonClick() {
-    this.withBorder = !this.withBorder;
+    this.text = "Changed From Hi Component";
+    this.clickBtn.emit(this.text);
   }
   onTextMouseOver() {
     this.textColor = "green";
@@ -18,6 +62,4 @@ export class HiComponent implements OnInit {
     this.textColor = "tomato";
   }
   constructor() {}
-
-  ngOnInit(): void {}
 }
